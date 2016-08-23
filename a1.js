a9de8dab3c3b01da2c2293d5d9c9868c321bd224
suprabhasupi <suprabhasupi@gmail.com>
2016-08-23 13:35:01 +0530

    google graph and d3_js graph

---
+++ a1.js
@@ -0,0 +1,235 @@
+var nytg = nytg || {};
+nytg.budget_array_data = [{
+    "name": "Ketan",
+    "positions": {
+        "total": {
+            "x": 308,
+            "y": 229
+        },
+        "department": {
+            "x": 225,
+            "y": 476
+        }
+    },
+    "id": 302,
+    "budget_2012": 66161000,
+    "change": 0.14342286241139,
+    "budget_2013": 75650000,
+    "department": "Veterans Affairs",
+    "discretion": "Mandatory"
+}, {
+    "name": "Jessienda",
+    "positions": {
+        "total": {
+            "x": 570,
+            "y": 340
+        },
+        "department": {
+            "x": 248,
+            "y": 434
+        }
+    },
+    "id": 285,
+    "budget_2012": 53981000,
+    "change": 0.0424038087475223,
+    "budget_2013": 56270000,
+    "department": "Veterans Affairs",
+    "discretion": "Discretionary"
+}, {
+    "name": "Jyothi",
+    "positions": {
+        "total": {
+            "x": 396,
+            "y": 114
+        },
+        "department": {
+            "x": 95,
+            "y": 442
+        }
+    },
+    "id": 195,
+    "budget_2012": 21000,
+    "change": 2.0,
+    "budget_2013": 63000,
+    "department": "Agriculture",
+    "discretion": "Discretionary"
+}, {
+    "name": "Suprabha",
+    "positions": {
+        "total": {
+            "x": 396,
+            "y": 114
+        },
+        "department": {
+            "x": 95,
+            "y": 442
+        }
+    },
+    "id": 195,
+    "budget_2012": 21000,
+    "change": 2.0,
+    "budget_2013": 63000,
+    "department": "Agriculture",
+    "discretion": "Discretionary"
+}];
+nytg.category_data = [{
+    "label": "ketan",
+    "total": 100,
+    "num_children": 26,
+    "short_label": "Health and Human Services"
+}, {
+    "label": "Agriculture",
+    "total": 154667000,
+    "num_children": 45,
+    "short_label": "Agriculture"
+}, {
+    "label": "Veterans Affairs",
+    "total": 137381000,
+    "num_children": 9,
+    "short_label": "Veterans Affairs"
+}, {
+    "label": "Justice",
+    "total": 30023000,
+    "num_children": 18,
+    "short_label": "Justice"
+}, {
+    "label": "Interior",
+    "total": 11357000,
+    "num_children": 31,
+    "short_label": "Interior"
+}, {
+    "label": "Commerce",
+    "total": 9239000,
+    "num_children": 21,
+    "short_label": "Commerce"
+}, {
+    "label": "Labor",
+    "total": 88993000,
+    "num_children": 15,
+    "short_label": "Labor"
+}, {
+    "label": "Homeland Security",
+    "total": 45109000,
+    "num_children": 23,
+    "short_label": "Homeland Security"
+}, {
+    "label": "Housing and Urban Development",
+    "total": 44010000,
+    "num_children": 14,
+    "short_label": "Housing"
+}, {
+    "label": "Corps of Engineers--Civil Works",
+    "total": 4668000,
+    "num_children": 3,
+    "short_label": "Corps of Engineers"
+}, {
+    "label": "Executive Office of the President",
+    "total": 392000,
+    "num_children": 12,
+    "short_label": "Office of the President"
+}, {
+    "label": "Energy",
+    "total": 32300000,
+    "num_children": 10,
+    "short_label": "Energy"
+}, {
+    "label": "Transportation",
+    "total": 74280000,
+    "num_children": 22,
+    "short_label": "Transportation"
+}, {
+    "label": "Education",
+    "total": 55685000,
+    "num_children": 15,
+    "short_label": "Education"
+}, {
+    "label": "Federal Deposit Insurance Corporation",
+    "total": 1515000,
+    "num_children": 5,
+    "short_label": "F.D.I.C."
+}, {
+    "label": "District of Columbia",
+    "total": 902000,
+    "num_children": 5,
+    "short_label": "District of Columbia"
+}, {
+    "label": "Environmental Protection Agency",
+    "total": 8138000,
+    "num_children": 2,
+    "short_label": "E.P.A."
+}, {
+    "label": "Defense - Military",
+    "total": 620259000,
+    "num_children": 13,
+    "short_label": "Defense"
+}, {
+    "label": "Institute of Museum and Library Services",
+    "total": 231000,
+    "num_children": 2,
+    "short_label": "Museum and Library Services"
+}, {
+    "label": "National Aeronautics and Space Administration",
+    "total": 17693000,
+    "num_children": 2,
+    "short_label": "NASA"
+}, {
+    "label": "National Archives and Records Administration",
+    "total": 370000,
+    "num_children": 3,
+    "short_label": "National Archives"
+}, {
+    "label": "National Science Foundation",
+    "total": 7470000,
+    "num_children": 2,
+    "short_label": "N.S.F."
+}, {
+    "label": "Nuclear Regulatory Commission",
+    "total": 127000,
+    "num_children": 2,
+    "short_label": "Nuclear Regulation"
+}, {
+    "label": "Office of Personnel Management",
+    "total": 94857000,
+    "num_children": 3,
+    "short_label": "Personnel Management"
+}, {
+    "label": "Postal Service",
+    "total": 78000,
+    "num_children": 2,
+    "short_label": "Postal Service"
+}, {
+    "label": "Public Company Accounting Oversight Board",
+    "total": 237000,
+    "num_children": 2,
+    "short_label": "Accounting Oversight"
+}, {
+    "label": "Railroad Retirement Board",
+    "total": 7202000,
+    "num_children": 3,
+    "short_label": "Railroad Retirement"
+}, {
+    "label": "Small Business Administration",
+    "total": 1111000,
+    "num_children": 2,
+    "short_label": "Small Business"
+}, {
+    "label": "Social Security Administration",
+    "total": 885315000,
+    "num_children": 2,
+    "short_label": "Social Security"
+}, {
+    "label": "Federal Communications Commission",
+    "total": 9633000,
+    "num_children": 2,
+    "short_label": "F.C.C."
+}, {
+    "label": "Securities Investor Protection Corporation",
+    "total": 259000,
+    "num_children": 2,
+    "short_label": "S.I.P.C."
+}, {
+    "label": "Other",
+    "total": -512596000,
+    "num_children": 97,
+    "short_label": "Other"
+}];
