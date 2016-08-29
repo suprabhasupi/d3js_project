277402247b680908477a50e6e45853d72b6e1351
suprabhasupi <suprabhasupi@gmail.com>
2016-08-29 14:46:25 +0530

    text transition chnaged

--- d3_js.js
+++ d3_js.js
@@ -21,7 +21,7 @@
 
 
  // selecting body container for SVG
- var id = 2;
+ // var id = 2;
  var svgContainer = d3.select(".add").append("svg")
      .attr("width", 900)
      .attr("height", 400)
@@ -125,15 +125,6 @@
              .enter()
              .append("text");
 
-         // text
-         //     .attr("x", function(d) {
-         //         return d.x_axis;
-         //     })
-         //     .attr("dy", function(d) {
-         //         return d.y_axis + 7;
-         //     })
-         //     .attr("transform", "scale(0)");
-
          text
              .attr("x", function(d) {
                  return d.x_axis;
@@ -215,7 +206,7 @@
      .transition()
 
      // .delay(950)
-     .duration(1000)
+     .duration(1500)
      .style("font-size", function(d) {
          return Math.min(2 * d.radius, (2 * d.radius - 8) / this.getComputedTextLength() * 12) + "px";
      })
@@ -227,83 +218,10 @@
  drawCirc(circle1);
 
 
- // var showAmb = function() {
-
- //     var circles = svgContainer.selectAll("circle")
- //         .data(jsonCircles)
- //         .enter()
- //         .append("circle")
- //         // .on("mouseover", function(d){
- //         //  d3.select(this).style("fill", "pink");
- //         // })
- //         .on('mouseover', tip.show)
- //         .on('mouseout', tip.hide);
- //     // .on("mouseout", function(d){
- //     //  d3.select(this).style("fill", "trasparent");
- //     // });
-
- //get the attribute
-
- //     var circleAttributes = circles
- //         .transition()
- //         .duration(750)
- //         .delay(500)
- //         .ease("bounce")
- //         .attr("cx", function(d) {
- //             return d.x_axis;
- //         })
- //         .attr("cy", function(d) {
- //             return d.y_axis;
- //         })
- //         .attr("r", function(d) {
- //             return d.radius;
- //         })
- //         .style("fill", function(d) {
- //             return d.color;
- //         });
-
-
- // }
-
  d3.select('#opts')
      .on('change', function() {
          var newData = eval(d3.select(this).property('value'));
          drawCirc(newData); // Call with new selection.
      });
 
- // var circleAttributes=circles
- // .transition()
- //   .duration(750)
- //   .delay(500)
- //  .ease("bounce")
- //   .attr("cx",function(d){return d.x_axis;})
- //                             .attr("cy",function(d){return d.y_axis;})
- //                             .attr("r",function(d){return d.radius;})
- //                             .style("fill",function(d){return d.color;});
-
-
- //                         .on("mouseover", function(d){
- //     d3.select(this).style("fill", "pink");
- // });
-
- //                             .on('mouseover', function(d) {
- //   d3.select(this).transition()
- //     .ease('cubic-out')
- //     .duration('200')
- //     .attr('font-size', 32)
- //     .attr('fill', 'springgreen');
- // })
- // .on('mouseout', function(d) {
- //   d3.select(this).transition()
- //     .ease('cubic-out')
- //     .duration('200')
- //     .attr('font-size', 20)
- //     .attr('fill', '#333');
- // });
-
-
-
-
-
- // .ease("bounce") or .ease("elastic") after .transition() you can achieve that effect. note that bounce and elastic are similar but not the same.
- // </script>
+ 
